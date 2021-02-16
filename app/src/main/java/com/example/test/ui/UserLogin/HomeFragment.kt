9a2344b89f34e01d11973920d2f30dea0e6554a9
paredes.llerena.leandro.joseph <URCqNeZR>
2021-02-17 00:56:45 +0100

    test

---
+++ app/src/main/java/com/example/test/ui/UserLogin/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.example.test.ui.UserLogin
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.test.R
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        return   inflater.inflate(R.layout.inicio, container, false)
+    }
+}
